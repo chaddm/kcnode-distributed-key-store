61a451fe86e594040b615e3afd57f49ec0ebc380
Chad Dumler-Montplaisir <chad.dumler.montplaisir@gmail.com>
2014-04-15 20:53:28 -0500

    Add IO

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ app.use(express.static('client'), {
   maxAge: 1000 * 60 * 60 * 24 * 365 * 20
 });
 
-app.io.route('share', function (req) {
-  req.io.broadcast('update', {
-    message: req.data
-  });
-});
 
 app.http();
 app.io();
+
+app.io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 app.listen(3000);
